2a6d775e8f812500092badffe11347e5793d254b
jumang4423 <catontheskate4423@gmail.com>
2022-05-16 15:50:39 +0900

    nixでも起動できる

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {}}:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    rustc
+    cargo
+    rustfmt
+    rust-analyzer
+    clippy
+    openssl
+  ];
+
+  RUST_BACKTRACE = 1;
+}
